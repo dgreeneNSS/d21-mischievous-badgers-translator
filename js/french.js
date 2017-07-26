254b1c90816d83ca70e1a8c17d20373b236e5f39
Daniel Greene <d.b.greene101@gmail.com>
2017-07-26 11:03:58 -0500

    voices

--- js/french.js
+++ js/french.js
@@ -29,6 +29,12 @@ var Translator = (function() {
         });
 
         console.log("new sentence: ", newSentence.join(" "));
+        let output_div = document.querySelector('#output-language');
+        output_div.innerText = newSentence.join(' ');
+        let speak = document.querySelector('#text-speak');
+        let speak_att = document.createAttribute('onclick');
+        speak_att.value = responsiveVoice.speak(output_div.innerText, 'French Female', {pitch: 0}, {rate: 0});
+        speak.setAttributeNode(speak_att);
     }
         
     };
