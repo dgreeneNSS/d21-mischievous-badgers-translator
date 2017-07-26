9e08ab0aba18aef52934db6beaa5c09887af6bdb
Daniel Greene <d.b.greene101@gmail.com>
2017-07-26 09:38:47 -0500

    finished emoji

--- js/emoji.js
+++ js/emoji.js
@@ -1,7 +1,42 @@
-😁 U+1F601	
-🎅 U+1F385	
-🎄 U+1F384
-➕ U+2795
-😊 U+1F60A
-🆕 U+1F195	
-🗓 U+1F5D3	
+var Translator = (function(e) {
+
+    let toBeDone = document.getElementById("toBeTranslated").value;
+    console.log(toBeDone);
+    let sentenceArray = toBeDone.toLowerCase().split(" ");
+    console.log(sentenceArray);
+    let newSentence = [];
+
+
+
+        e.addEmoji = function() {
+            sentenceArray.forEach(function(item){
+                if(item == "merry"){
+                    newSentence.push("😁");
+                }else if(item == "christmas"){
+                    newSentence.push('🎄🎅');
+                }else if(item == 'and'){
+                    newSentence.push('➕');
+                }else if(item == "happy"){
+                    newSentence.push("😊");
+                }else if(item == 'new'){
+                    newSentence.push('🆕 ');
+                }else if(item == 'year'){
+                    newSentence.push('🗓');
+                }
+
+                return newSentence.join(" ");
+            });
+
+            let output_div = document.querySelector('#output-language');
+            output_div.innerText = newSentence.join(' ');
+            let speak = document.querySelector('#text-speak');
+            let speak_att = document.createAttribute('onclick');
+            speak_att.value = responsiveVoice.speak(output_div.innerText, 'Czech Female', {pitch: 0}, {rate: 0});
+            speak.setAttributeNode(speak_att);
+        }
+
+        return e
+    }
+
+
+)(Translator);
