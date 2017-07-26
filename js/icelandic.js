43ca59d02925170a5135df7d25c084ea52ea0771
daniel-certa-1228 <mfdancerta@gmail.com>
2017-07-26 09:38:45 -0500

    finish icelandic

--- js/icelandic.js
+++ js/icelandic.js
@@ -1,39 +1,42 @@
-console.log( "icelandic.js" );
+var Translator = (function(e) {
 
-var Translator = (function() {
+    let toBeDone = document.getElementById("toBeTranslated").value;
+    console.log(toBeDone);
+    let sentenceArray = toBeDone.toLowerCase().split(" ");
+    console.log(sentenceArray);
+    let newSentence = [];
 
-	let toBeTrans = toBeTranslated.value.toLowerCase();
-	console.log( "toBeTrans", toBeTrans );
-	let sentenceArray = toBeTrans.split(" ");
-	console.log( "sentenceArray", sentenceArray );
-	let newSentence = [];
-	console.log( "newSentence", newSentence );
 
-	return {
-		addIcelandic: function() {
 
-			sentenceArray.forEach( (item) => {
+        e.addIcelandic = function() {
+            sentenceArray.forEach(function(item){
+                if(item == "merry"){
+                    newSentence.push("gleðileg");
+                }else if(item == "christmas"){
+                    newSentence.push("jól");
+                }else if(item == 'and'){
+                    newSentence.push('og');
+                }else if(item == "happy"){
+                    newSentence.push("farsælt");
+                }else if(item == 'new'){
+                    newSentence.push('komandi');
+                }else if(item == 'year'){
+                    newSentence.push('ár');
+                }
 
-				if (item === "merry") {
-					newSentence.push("gleðileg");
-				}  else if  (item === "christmas") {
-					newSentence.push("jól");
-				}  else if (item === "and") {
-					newSentence.push("og");
-				}  else if (item === "happy") {
-					newSentence.push("farsælt")
-				}  else if (item === "new") {
-					newSentence.push("komandi");
-				}  else if (item === "year") {
-					newSentence.push("ár");
-				}
-				return newSentence.join(" ");
-			})
+                return newSentence.join(" ");
+            });
 
-			console.log( "newSentence", newSentence.join(" ") );
-			
-		}
+            let output_div = document.querySelector('#output-language');
+            output_div.innerText = newSentence.join(' ')
+            let speak = document.querySelector('#text-speak')
+            let speak_att = document.createAttribute('onclick')
+            speak_att.value = responsiveVoice.speak(output_div.innerText, 'Icelandic Male', {pitch: 0}, {rate: 0})
+            speak.setAttributeNode(speak_att)
+        }
 
-	}
+        return e
+    }
 
-})();
+
+)(Translator);
